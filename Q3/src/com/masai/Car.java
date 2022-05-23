8f2193ababbed69c09aa2e91fdefe3d5ead3b5d6
Ram <ss4033167@gmail.com>
2022-05-23 15:54:31 +0530

    evalution

---
+++ Q3/src/com/masai/Car.java
@@ -0,0 +1,21 @@
+package com.masai;
+
+public class Car {
+	String model;
+
+	String companyName;
+	String Color;
+	Engine engine;
+	public Car() {
+		
+	}
+	public Car(String model, String companyName, String color, Engine engine) {
+		
+		this.model = model;
+		this.companyName = companyName;
+		this.Color = color;
+		this.engine=engine;
+		
+	}
+	
+}
---
+++ Q3/src/com/masai/Engine.java
@@ -0,0 +1,12 @@
+package com.masai;
+
+public class Engine {
+	int rmp;
+
+	int Power;
+	String manufacturer;
+	Boolean hasTurbo;
+	public void enableTurbo() {
+		hasTurbo=true;
+	}
+}
